6431dd8012a51b49de19f16f0f9bfaa44d51a023
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-08 08:53:41 -0300

    gengar duki

--- lol/src/Components/Context/CreacionesContext.jsx
+++ lol/src/Components/Context/CreacionesContext.jsx
@@ -7,10 +7,11 @@ export const FavoritosContext = createContext(null)
 export const FavoritosProvider = ({ children }) => {
 
     const [FavoritosG, setFavoritosG] = useState(JSON.parse(localStorage.getItem('Favoritos')) || null)
-
+    //La de idea de los usuarios es hacer una lista de favoritos x usuario. Hacer un objeto usuario que tenga mail y la lista de favoritos.
+    const [usuarios, setUsuarios] = useState() 
     
     return (
-        <FavoritosContext.Provider value={{FavoritosG, setFavoritosG}}>
+        <FavoritosContext.Provider value={{FavoritosG, setFavoritosG, usuarios, setUsuarios}}>
             {children}
         </FavoritosContext.Provider>
     )
